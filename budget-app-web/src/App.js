2cb856417d005243ca68543b820a91b1547e9c4d
AirFusion <jfang.cv.ca.us@gmail.com>
2021-06-30 21:13:54 -0700

    check into git as backup

---
+++ budget-app-web/src/App.js
@@ -0,0 +1,10 @@
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="Container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ budget-app-web/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className='container'>
+            <br></br>
+            <br></br>
+            <h1>Expenses</h1>
+        </div>
+    )
+}
+export default Header
